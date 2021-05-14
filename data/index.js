deca3072ee59f88f57ac734ce09dab9deaa3e478
Snehil <spate184@stevens.edu>
2021-05-13 23:03:59 -0400

    deleted old codebase

--- data/index.js
+++
@@ -1,9 +0,0 @@
-const userData = require('./users');
-const loginData = require('./login');
-const logoutData = require('./logout');
-
-module.exports = {
-  users: userData,
-  login: loginData,
-  logout: logoutData
-};
